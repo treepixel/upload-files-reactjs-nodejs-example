af86980ab45f9cdca9fbc325111ccfe2bfae946d
Antônio Rodrigues <antonio.rodrigues_tga@hotmail.com>
2019-06-05 11:57:43 -0300

    feat: Setup the mongodb and add the PostSchema

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const routes = require('./routes');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//Database setup
+mongoose.connect('mongodb://localhost:27017/upload_db', {
+  useNewUrlParser: true
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
