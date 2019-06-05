43eb67d41f3bd26b43c11e53c4e5acfad93b0f87
Antônio Rodrigues <antonio.rodrigues_tga@hotmail.com>
2019-06-05 12:11:38 -0300

    feat: Save info files in Posts Collection  on upload files

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,10 +2,19 @@ const routes = require('express').Router();
 const multer = require('multer');
 const multerConfig = require('./config/multer');
 
-routes.post('/posts', multer(multerConfig).single('file'), (req, res) => {
-  console.log(req.file);
+const Post = require('./models/Post');
 
-  return res.json({ hello: 'teste' });
+routes.post('/posts', multer(multerConfig).single('file'), async (req, res) => {
+  const { originalname: name, size, filename: key } = req.file;
+
+  const post = await Post.create({
+    name,
+    size,
+    key,
+    url: ''
+  });
+
+  return res.json(post);
 });
 
 module.exports = routes;
