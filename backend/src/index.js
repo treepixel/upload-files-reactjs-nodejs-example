dce9b608e2fe755dc1cc9bb166577f5950a67670
Antônio Rodrigues <antonio.rodrigues_tga@hotmail.com>
2019-06-06 11:41:27 -0300

    feat: Install dependencies aws-sdk, multers3, config multer.js and add routes to list and delete files uploaded

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,20 @@
-const express = require('express');
-const routes = require('./routes');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
+require("dotenv").config();
+
+const express = require("express");
+const routes = require("./routes");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 const app = express();
 
 //Database setup
-mongoose.connect('mongodb://localhost:27017/upload_db', {
+mongoose.connect("mongodb://localhost:27017/upload_db", {
   useNewUrlParser: true
 });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 app.use(routes);
 
