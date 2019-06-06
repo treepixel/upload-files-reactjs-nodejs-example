8ad08cba6fe152159c549d78ee27d33479549733
Antônio Rodrigues <antonio.rodrigues_tga@hotmail.com>
2019-06-06 14:41:49 -0300

    feat: Get post of uploaded files on componentDidMout of App.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ const routes = require("./routes");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
@@ -13,6 +14,7 @@ mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
+import { uniqueId } from "lodash";
+import filesize from "filesize";
+
+import api from "./services/api";
 
 import GlobalStyle from "./styles/global";
 import { Container, Content } from "./styles";
 
 import Upload from "./components/Upload";
+import FileList from "./components/FileList";
 
-function App() {
-  return (
-    <Container>
-      <Content>
-        <Upload />
-      </Content>
-      <GlobalStyle />;
-    </Container>
-  );
+class App extends Component {
+  state = {
+    uploadedFiles: []
+  };
+
+  async componentDidMount() {
+    const response = await api.get("posts");
+
+    this.setState({
+      uploadedFiles: response.data.map(file => ({
+        id: file._id,
+        name: file.name,
+        readableSize: filesize(file.size),
+        preview: file.url,
+        uploaded: true,
+        url: file.url
+      }))
+    });
+  }
+
+  render() {
+    const { uploadedFiles } = this.state;
+    return (
+      <Container>
+        <Content>
+          <Upload />
+          {!!uploadedFiles.length && (
+            <FileList files={uploadedFiles} onDelete={() => {}} />
+          )}
+        </Content>
+        <GlobalStyle />;
+      </Container>
+    );
+  }
 }
 
 export default App;
